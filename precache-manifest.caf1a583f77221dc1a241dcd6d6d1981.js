4c1318ec2f9e8f4b78dbfc2e71e4cf340d5933dc
jameswassink <james.wassink@gmail.com>
2019-02-26 15:21:58 +1100

    Updates

---
+++ precache-manifest.caf1a583f77221dc1a241dcd6d6d1981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01a4b996e22ca208b3ce1df361e06f68",
+    "url": "/simple-scavenger-hunt/static/media/tea.01a4b996.jpg"
+  },
+  {
+    "revision": "c8271b158f76bc2e5605",
+    "url": "/simple-scavenger-hunt/static/js/runtime~main.c8271b15.js"
+  },
+  {
+    "revision": "5d6581ca8fab7b040c4b",
+    "url": "/simple-scavenger-hunt/static/js/main.5d6581ca.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/simple-scavenger-hunt/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "5d6581ca8fab7b040c4b",
+    "url": "/simple-scavenger-hunt/static/css/main.f2f88e00.chunk.css"
+  },
+  {
+    "revision": "b57288a299d75f6c8215555ba5137b3e",
+    "url": "/simple-scavenger-hunt/index.html"
+  }
+];
